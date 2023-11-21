6707f76c29ab968b4f810e95f7432a8a818fafce
Laurent Broudoux <laurent.broudoux@gmail.com>
2023-11-21 09:43:11 +0100

    chore: Bump to latest microcks-testcontainers-java and add MicrocksContainersEnsemble sample Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

--- shift-left-demo/spring-boot-order-service/src/test/java/org/acme/order/api/OrderControllerPostmanContractTests.java
+++ shift-left-demo/spring-boot-order-service/src/test/java/org/acme/order/api/OrderControllerPostmanContractTests.java
@@ -1,21 +1,19 @@
 package org.acme.order.api;
 
-import io.github.microcks.testcontainers.MicrocksContainer;
+import io.github.microcks.testcontainers.MicrocksContainersEnsemble;
 import io.github.microcks.testcontainers.model.TestRequest;
 import io.github.microcks.testcontainers.model.TestResult;
 import io.github.microcks.testcontainers.model.TestRunnerType;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-
-
 import org.testcontainers.Testcontainers;
 import org.testcontainers.junit.jupiter.Container;
 
@@ -26,10 +24,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @org.testcontainers.junit.jupiter.Testcontainers
-public class OrderControllerContractTests {
+public class OrderControllerPostmanContractTests {
 
    @Container
-   public static MicrocksContainer microcksContainer = new MicrocksContainer("quay.io/microcks/microcks-uber:nightly")
+   public static MicrocksContainersEnsemble microcksEnsemble = new MicrocksContainersEnsemble("quay.io/microcks/microcks-uber:1.8.0")
          .withAccessToHost(true);
 
    @LocalServerPort
@@ -38,10 +36,11 @@ public class OrderControllerContractTests {
    @BeforeAll
    public static void setup() throws Exception {
       // Load the System Under Test OpenAPI contract into Microcks.
-      microcksContainer.importAsMainArtifact(new File("target/classes/order-service-openapi.yaml"));
+      microcksEnsemble.getMicrocksContainer().importAsMainArtifact(new File("target/classes/order-service-openapi.yaml"));
+      microcksEnsemble.getMicrocksContainer().importAsSecondaryArtifact(new File("target/classes/order-service-postman-collection.json"));
       // Load also the third-party ones so that RestTemplate will also use Microcks mocks 👻
-      microcksContainer.importAsMainArtifact(new File("target/test-classes/third-parties/apipastries-openapi.yaml"));
-      microcksContainer.importAsSecondaryArtifact(new File("target/test-classes/third-parties/apipastries-postman-collection.json"));
+      microcksEnsemble.getMicrocksContainer().importAsMainArtifact(new File("target/test-classes/third-parties/apipastries-openapi.yaml"));
+      microcksEnsemble.getMicrocksContainer().importAsSecondaryArtifact(new File("target/test-classes/third-parties/apipastries-postman-collection.json"));
    }
 
    @BeforeEach
@@ -52,22 +51,24 @@ public class OrderControllerContractTests {
 
    @DynamicPropertySource
    static void configureProperties(DynamicPropertyRegistry registry) {
-      String url = microcksContainer.getRestMockEndpoint("API Pastries", "0.0.1");
+      String url = microcksEnsemble.getMicrocksContainer().getRestMockEndpoint("API Pastries", "0.0.1");
       registry.add("pastries.baseUrl", () -> url);
    }
 
    @Test
-   public void testOpenAPIContract() throws Exception {
-      // Ask for an Open API conformance to be launched.
+   public void testPostmanCollectionContract() throws Exception {
+      // Ask for a Postman Collection script conformance to be launched.
       TestRequest testRequest = new TestRequest.Builder()
             .serviceId("Order Service API:0.1.0")
-            .runnerType(TestRunnerType.OPEN_API_SCHEMA.name())
+            .runnerType(TestRunnerType.POSTMAN.name())
             .testEndpoint("http://host.testcontainers.internal:" + port + "/api")
             .build();
 
-      TestResult testResult = microcksContainer.testEndpoint(testRequest);
+      TestResult testResult = microcksEnsemble.getMicrocksContainer().testEndpoint(testRequest);
 
       // You may inspect complete response object with following:
+      System.err.println(microcksEnsemble.getMicrocksContainer().getLogs());
+      System.err.println(microcksEnsemble.getPostmanContainer().getLogs());
       ObjectMapper mapper = new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
       System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(testResult));
 
